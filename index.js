872ebb5b3963d6acf5181bdc6bf9b68f0ea0a2bc
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-16 23:35:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,31 +15,11 @@ function johnLennonFacts(array) {
 return newarray;
 }
 
-function iLoveTheBeatles(number){
+function iLoveTheBeatles(number) {
   var newarray = [];
-  
   do{
     newarray.push("I love the Beatles!");
     number++;
   } while (number < 15);
-  
   return newarray;
-  
 }
-
-
-/// var bio = " is the co-founder and editress-in-chief of The Huffington Post";
-/// console.log(entrepreneurs[2] + bio);
-
-
-//var entrepreneurs = ["Elizabeth Holmes", "Laurene Powell Jobs", "Arianna Huffington"];
- 
-// the line below will print the string "Elizabeth Holmes"
-//console.log(entrepreneurs[0]);
- 
-// the code below will print the string "Arianna Huffington is the co-founder and editress-in-chief of The Huffington Post"
-//var bio = " is the co-founder and editress-in-chief of The Huffington Post";
-//console.log(entrepreneurs[2] + bio);
- 
-// the line below will return undefined
-//entrepreneurs[9];
